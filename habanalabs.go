b672cf80993479a8f5abed97eb81e037148786c9
Robert F. Dickerson <rdickerson@habana.ai>
2021-01-28 17:17:09 -0600

    remove resolvable dependencies

--- habanalabs.go
+++ habanalabs.go
@@ -23,7 +23,7 @@ import (
 	"strings"
 	"time"
 
-	pluginapi "k8s.io/kubernetes/pkg/kubelet/apis/deviceplugin/v1beta1"
+	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
 )
 
 type DevID string
--- hlml.go
+++ hlml.go
@@ -29,7 +29,7 @@ import (
 	"strings"
 	"unsafe"
 
-	pluginapi "k8s.io/kubernetes/pkg/kubelet/apis/deviceplugin/v1beta1"
+	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
 )
 
 const (
